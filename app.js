d52636f34af6d07623cb7c4f3b89e0a6674e29fe
FaridFarahmand <faridfarahmandf2f@gmail.com>
2022-09-13 12:39:54 +0430

    toggling the div

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const books = new Vue({
     data: {
         title: "The imaginary library",
         author: "Matt Heige",
+        summary: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur libero ipsa, illum ea rerum aliquam quis optio omnis. Inventore, laboriosam.",
+        toggleShow: true,
     },
     methods: {
-        clickHandler: function() {
-            alert(this.title);
+        // clickHandler: function() {
+        //     alert(this.title);
+        // }
+        //lesson 5
+        toggleHandler: function() {
+            this.toggleShow = !this.toggleShow;
         }
     }
-})
+});
