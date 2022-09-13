9305bc9495bfdb98c24eb4895406c6fe3557644d
FaridFarahmand <faridfarahmandf2f@gmail.com>
2022-09-13 11:08:40 +0430

    creating multiple vue instance

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const HelloVue = new Vue({
+    //element
     el:"#helloWorld",
     data: {
         title: "Hello Vue Project!",
         message: "this is the first real vue project"
+    }
+})
+
+// lesson2 multiple vue constructor 
+const example = new Vue({
+    el:"#example",
+    data: {
+        title:"Hello",
+        message:"this is the example",
+        name: "Farid",
     }
 })
