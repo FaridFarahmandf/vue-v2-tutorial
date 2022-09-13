c2829d9441de0e7eb3c909a1f30b32a074aad4e1
FaridFarahmand <faridfarahmandf2f@gmail.com>
2022-09-13 12:08:39 +0430

    onClick

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-const HelloVue = new Vue({
-    //element
-    el:"#helloWorld",
+//lesson4
+const books = new Vue({
+    el:"#books",
     data: {
-        title: "Hello Vue Project!",
-        message: "this is the first real vue project"
-    }
-})
-
-// lesson2 multiple vue constructor 
-const example = new Vue({
-    el:"#example",
-    data: {
-        title:"Hello",
-        message:"this is the example",
-        name: "Farid",
-        //lesson3
-        img: {
-            src:"./assets/images/cat1.jpg",
-            alt:"the pretty cat"
+        title: "The imaginary library",
+        author: "Matt Heige",
+    },
+    methods: {
+        clickHandler: function() {
+            alert(this.title);
         }
     }
 })
